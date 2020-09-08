9b875bada730762946265923b60643510c5ec803
ilima12091 <limaignacio12091@gmail.com>
2020-09-08 19:46:52 -0300

    Agrego el trabajo

---
+++ src/Program/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace PII_Game_Of_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] b = Logica.ProximaGeneracion(); //variable que representa el tablero
+            int width = b.GetLength(0); //variabe que representa el ancho del tablero
+            int height = b.GetLength(1); //variabe que representa altura del tablero
+            while (true)
+            {
+                Console.Clear();
+                StringBuilder s = new StringBuilder();
+                for (int y = 0; y<height;y++)
+                {
+                    for (int x = 0; x<width; x++)
+                    {
+                        if(b[x,y])
+                        {
+                            s.Append("|X|");
+                        }
+                        else
+                        {
+                            s.Append("___");
+                        }
+                    }
+                    s.Append("\n");
+                }
+                Console.WriteLine(s.ToString());
+                //=================================================
+                //Invocar método para calcular siguiente generación
+                //=================================================
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
